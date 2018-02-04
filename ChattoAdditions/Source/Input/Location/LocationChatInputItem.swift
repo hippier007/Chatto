a4f9492b6b6a2695e09a0c467fbc3e8e88add83f
Ninh Vo <hippier007@gmail.com>
2018-02-04 14:05:02 +0700

    Location input item

---
+++ ChattoAdditions/Source/Input/Location/LocationChatInputItem.swift
@@ -0,0 +1,56 @@
+//
+//  LocationChatInputItem.swift
+//  ChattoAdditions
+//
+//  Created by Ninh Vo on 2/4/18.
+//  Copyright © 2018 Badoo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol LocationChatInputDelegate: class {
+    func inputView(_ inputView: PhotosInputViewProtocol, didSelectImage image: UIImage)
+}
+
+open class LocationChatInputItem: ChatInputItemProtocol {
+    typealias Class = LocationChatInputItem
+    
+    public var inputView: UIView?
+    
+    open var tabView: UIView {
+        return self.internalTabView
+    }
+    
+    open var presentationMode: ChatInputItemPresentationMode
+    
+    open var showsSendButton: Bool {
+        return true
+    }
+    
+    open var selected: Bool = false
+    
+    lazy private var internalTabView: UIButton = {
+        return TabInputButton.makeInputButton(withAppearance: self.buttonAppearance, accessibilityID: "location.chat.input.view")
+    }()
+    
+    public func handleInput(_ input: AnyObject) {
+        print("Location button tap...")
+    }
+    
+    let buttonAppearance: TabInputButtonAppearance
+    
+    public init(tabInputButtonAppearance: TabInputButtonAppearance = Class.createDefaultButtonAppearance()){
+        self.buttonAppearance = tabInputButtonAppearance
+        self.presentationMode = .none
+    }
+    
+    public static func createDefaultButtonAppearance() -> TabInputButtonAppearance {
+        let images: [UIControlStateWrapper: UIImage] = [
+            UIControlStateWrapper(state: .normal): UIImage(named: "send_gps", in: Bundle(for: Class.self), compatibleWith: nil)!,
+            UIControlStateWrapper(state: .selected): UIImage(named: "send_gps", in: Bundle(for: Class.self), compatibleWith: nil)!,
+            UIControlStateWrapper(state: .highlighted): UIImage(named: "send_gps", in: Bundle(for: Class.self), compatibleWith: nil)!
+        ]
+        return TabInputButtonAppearance(images: images, size: nil)
+    }
+}
